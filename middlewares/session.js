378c43075715b86202012e6b75ec59d779d802d5
Gao Dechen <gaodechen@live.cn>
2019-05-07 23:52:22 +0800

    update

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,10 +1,13 @@
 // const session = require('express-session');
 // const sessionStore = require('connect-redis')(session)
 // const redis = require('redis')
-// const { SESSION_STORAGE } = require('../config')
+const { REDIS } = require('../config')
 
 const redisSession = require('node-redis-session');
 
 module.exports = () => {
-	return redisSession({ cookieName: 'musicine_server_cookie'})
+	return redisSession({
+		cookieName: 'musicine_server_cookie',
+		redisOptions: [REDIS.RDS_PORT, REDIS.RDS_HOST],
+	})
 }
