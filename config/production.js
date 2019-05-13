a3f60788f848f3ef148700cb76b884ab7b501d29
Gao Dechen <gaodechen@live.cn>
2019-05-13 12:49:40 +0800

    update mongo uri

--- config/production.js
+++ config/production.js
@@ -7,8 +7,14 @@ exports.TEST_HOST_LIST = 'http://47.111.178.254'
 // address of static server
 exports.STATIC_SERVER = 'http://47.98.157.29:3002'
 // MongoDB config
+const MG_SET = [
+	'dds-bp19e8f6e0cf38f41.mongodb.rds.aliyuncs.com:3717',
+	'dds-bp19e8f6e0cf38f42.mongodb.rds.aliyuncs.com:3717',
+	'dds-bp19e8f6e0cf38f43.mongodb.rds.aliyuncs.com:3717',
+	'dds-bp19e8f6e0cf38f44.mongodb.rds.aliyuncs.com:3717',
+]
 exports.MONGODB = {
-    uri: 'mongodb://musicine.mongodb.rds.aliyuncs.com:3717,musicine1.mongodb.rds.aliyuncs.com:3717,musicine2.mongodb.rds.aliyuncs.com:3717,musicine3.mongodb.rds.aliyuncs.com:3717/musicine?replicaSet=dds-bp19e8f6e0cf38f4&readPreference=secondaryPreferred',
+	uri: 'mongodb://' + MG_SET.map(e => (e + ',')) + '/musicine?replicaSet=dds-bp19e8f6e0cf38f4&readPreference=secondaryPreferred',
 };
 
 // mongodb://root:xxxxxxxx@dds-xxxxxxxxxxxx:3717,xxxxxxxxxxxx:3717/admin?replicaSet=mgset-xxxxxx&readPreference=secondaryPreferred
@@ -20,8 +26,8 @@ const RDS_HOST = 'musicine.redis.rds.aliyuncs.com'
 
 // Redis config
 exports.REDIS = {
-    RDS_PORT,
-    RDS_HOST,
+	RDS_PORT,
+	RDS_HOST,
 	RDS_OPTS: {},
 }
 
